09d35918c29d9ae4174e84c070ba9d8e10679d73
Terry Mace <terry.mace@bglgroup.co.il>
2020-11-20 17:02:01 +0000

    Changed Fact to Theory and added tests for 0 and -1

--- unit-testing-using-dotnet-test/PrimeService.Tests/PrimeService_IsPrimeShould.cs
+++ unit-testing-using-dotnet-test/PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -5,13 +5,17 @@ namespace Prime.UnitTests.Services
 {
     public class PrimeService_IsPrimeShould
     {
-        [Fact]
-        public void IsPrime_InputIs1_ReturnFalse()
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
         {
             var primeService = new PrimeService();
-            bool result = primeService.IsPrime(1);
 
-            Assert.False(result, "1 should not be prime");
+            var result = primeService.IsPrime(value);
+
+            Assert.False(result, $"{value} should not be prime");
         }
     }
 }
